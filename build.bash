422510d8e840ddcc39c63f1b1aeac45c2b696afb
Enric Cervera <ecervera@uji.es>
2026-01-05 17:03:28 +0100

    Build without cache

--- build.bash
+++ build.bash
@@ -3,7 +3,7 @@
 BASE_IMAGE=unitree_mujoco_base
 IMAGE=unitree_mujoco
 
-docker build -t $BASE_IMAGE .
+docker build --no-cache -t $BASE_IMAGE .
 
 if [ $(docker images -q "$IMAGE") ]; then
   docker image rm $IMAGE
